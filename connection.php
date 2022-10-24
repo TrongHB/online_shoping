5f8d02e4649acf19c7e679c2111315ac885f95b7
TrongHB <tronghbgcc200034@fpt.edu.vn>
2022-10-24 17:45:46 +0700

    connect db

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'onlineshopping')
-            or die("Can not conncect database".mysqli_connect_error());
+$conn = pg_connect("postgres://yqlyidtbqsygjx:6585eb33cfdd7a8286439ef1469a03aeb193a22d50cc8180b36e30468e144d45@ec2-54-160-200-167.compute-1.amazonaws.com:5432/dcflavd47l2dgs");
+if (!$conn) {
+    die("Connection fail");
+}
 ?>
